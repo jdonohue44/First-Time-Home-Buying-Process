ded23f644fb2e26cf1e07ba77ae495d562792819
Jared Donohue <jared.donohue@gmail.com>
2025-04-29 17:51:07 -0400

    Add home page

--- server.py
+++ server.py
@@ -234,6 +234,10 @@ def quiz_result():
 def glossary_page():
     return render_template("cheatsheet.html", cheatsheet=cheatsheet)
 
+@app.route("/single-or-partnered")
+def single_or_partnered():
+    return render_template("single-or-partnered.html")
+
 @app.route("/timeline")
 def timeline():
     return render_template("timeline.html")
